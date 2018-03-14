9cc71e642d8a81f340ab983db27261fac91612fe
Axis256 <32141567+Axis256@users.noreply.github.com>
2018-03-14 21:57:37 +0300

    добавил средние коллизии и сравнения

--- lab1/HashTable.py
+++ lab1/HashTable.py
@@ -1,8 +1,10 @@
 class HashTable:
     def __init__(self):
-        self.size = 0
         self.capacity = 288
         self.table = [None] * self.capacity
+        self._collision_count = 0
+        self._comp_count = 0
+        self._search_count = 0
 
     def _hash_func(self, value):
         chr1 = value[0]
@@ -33,6 +35,7 @@ class HashTable:
         n = self.capacity
         i = self._hash_func(value)
         h0 = i
+        self._search_count += 1
         if self.table[i] is None:
             return None
         elif self.table[i] == value:
@@ -40,6 +43,8 @@ class HashTable:
         else:
             i = (i + pi) % n
             while i != h0:
+                self._collision_count += 1
+                self._comp_count += 1
                 if self.table[i] is None:
                     return None
                 elif self.table[i] == value:
@@ -48,3 +53,9 @@ class HashTable:
                     i = (i + pi) % n
             if i == h0:
                 return None
+
+    def avg_comp(self):
+        return self._comp_count / self._search_count
+
+    def avg_collisions(self):
+        return self._collision_count / self._search_count
