dbf55ad7c69c63b8c80cd14761506eef1f2968ae
Vladislav Lavrinenko <vlad.lavrinenko.99@mail.ru>
2018-04-26 14:31:30 +0300

    final(no)

--- lab2/dijkstra.py
+++ lab2/dijkstra.py
@@ -2,7 +2,7 @@ import timeit
 from heap import Heap
 
 
-def Dijkstra(edges, start, d):
+def Dijkstra_heap(edges, start, d):
     begin = timeit.default_timer()
 
     A = [None] * len(edges)
@@ -19,4 +19,25 @@ def Dijkstra(edges, start, d):
     # to give same result as original, assign zero distance to unreachable vertices
     # return [0 if x is None else x for x in A]
     end = timeit.default_timer()
-    return (end - begin)
+    return end - begin
+
+def Dijkstra(graph, start):
+    begin = timeit.default_timer()
+
+    size = len(graph)
+    valid = [True] * size
+    weight = [1000000] * size
+    weight[start] = 0
+    for i in range(size):
+        min_weight = 1000001
+        ID_min_weight = -1
+        for i in range(len(weight)):
+            if valid[i] and weight[i] < min_weight:
+                min_weight = weight[i]
+                ID_min_weight = i
+        for i in range(size):
+            if weight[ID_min_weight] + graph[ID_min_weight][i] < weight[i]:
+                weight[i] = weight[ID_min_weight] + graph[ID_min_weight][i]
+        valid[ID_min_weight] = False
+    end = timeit.default_timer()
+    return end - begin
--- lab2/main.py
+++ lab2/main.py
@@ -1,17 +1,23 @@
-from dijkstra import Dijkstra
+from dijkstra import Dijkstra, Dijkstra_heap
 from graphs import gen_graph, invert_graph_to_edges
 
 q = 1
 r = 10 ** 6
 
-with open('time_d4.txt', 'w') as file:
-    for n in range(100, 10001, 100):
-        m = int(n ** 2 / 10)
+with open('time_d4.txt', 'a') as file1:
+    with open('time_m.txt', 'a') as file2:
+        for n in range(100, 10001, 100):
+            m = int(n ** 2 / 10)
 
-        G = gen_graph(n, m, q, r)
+            G = gen_graph(n, m, q, r)
 
-        time = Dijkstra(invert_graph_to_edges(G), 0, 2)
+            time_m = Dijkstra(G, 0)
+            time_h = Dijkstra_heap(invert_graph_to_edges(G), 0, 4)
 
-        file.write(str(time) + ' ')
+            file1.write(str(time_h) + ' ')
+            file2.write(str(time_m) + ' ')
+            file1.flush()
+            file2.flush()
 
-        print("n : {}\t m : {}\t time: {}".format(n, m, time))
+            print("n : {}\t m : {}\t heap time: {}\t mark time: {}".format(n, m, time_h, time_m))
+            #print("n : {}\t m : {}\t mark time: {}".format(n, m, time_m))
